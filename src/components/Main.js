4799c4158f8ad0f09b161180c9233f83cfd66439
Spencer Marx <spencer.s.marx@gmail.com>
2019-08-15 19:41:33 -0400

    Add Netlify form attributes to rsvp article

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,7 +6,7 @@ import HotelOptions from './HotelOptions'
 import imageMoney from '../images/imageMoney.jpg'
 
 class Main extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       language: 'en',
@@ -14,14 +14,19 @@ class Main extends React.Component {
   }
   render() {
 
-    let close = <div className="close" onClick={() => {this.props.onCloseArticle()}}></div>
+    let close = <div className="close" onClick={() => { this.props.onCloseArticle() }}></div>
 
     return (
-      <div ref={this.props.setWrapperRef} id="main" style={this.props.timeout ? {display: 'flex'} : {display: 'none'}}>
+      <div ref={this.props.setWrapperRef} id="main" style={this.props.timeout ? { display: 'flex' } : { display: 'none' }}>
 
-<article id="rsvp" className={`${this.props.article === 'rsvp' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+        <article id="rsvp" className={`${this.props.article === 'rsvp' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{ display: 'none' }}>
           <h2 className="major">RSVP</h2>
-          <form method="post" action="#">
+          <form
+          name="rsvp"
+          method="post"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field">
+            <input type="hidden" name="form-name" value="rsvp" />
             <div className="field half first">
               <label htmlFor="name">Family Name</label>
               <input type="text" name="name" id="name" />
@@ -42,32 +47,32 @@ class Main extends React.Component {
           {close}
         </article>
 
-        <article id="where" className={`${this.props.article === 'where' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+        <article id="where" className={`${this.props.article === 'where' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{ display: 'none' }}>
           <h2 className="major">Where</h2>
           <p>We will be hosting both the ceremony and reception at the beautiful Plimouth Plantation. Autumn will have arrived, so be sure to bring a jacket!</p>
-          <div style={{width: '100%', height: '400px'}}>
-            <Map/>
+          <div style={{ width: '100%', height: '400px' }}>
+            <Map />
           </div>
 
           {close}
         </article>
 
-        <article id="lodging" className={`${this.props.article === 'lodging' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+        <article id="lodging" className={`${this.props.article === 'lodging' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{ display: 'none' }}>
           <h2 className="major">Lodging</h2>
           <p>Here is a list of affordable hotels and other accommodations close by the wedding venue. Feel free to check Airbnbs as well! We recommend that you book as soon as possible so you get a better rate. If you have any other questions, don't hesitate to reach out!</p>
-          <HotelOptions/>
+          <HotelOptions />
           {close}
         </article>
 
-        <article id="gifts" className={`${this.props.article === 'gifts' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+        <article id="gifts" className={`${this.props.article === 'gifts' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{ display: 'none' }}>
           <h2 className="major">Gifts</h2>
           <span className="image main"><img src={imageMoney} alt="" /></span>
           <p>
             Please do not feel obliged to buy us a gift; all that we are expecting is you. If you would like to give something, a contribution towards our future would be greatly appreciated! Thank you!
           </p>
-          <p style={{fontWeight: '600'}}> – Angie and Spencer Marx</p>
-          <p style={{textAlign: 'center', borderTop:'solid 1px white', margin: '0', paddingTop: '1rem'}}>37 Crescent Ave</p>
-          <p style={{textAlign: 'center', borderBottom:'solid 1px white', paddingBottom: '1rem'}}>Plymouth, MA 02360</p>
+          <p style={{ fontWeight: '600' }}> – Angie and Spencer Marx</p>
+          <p style={{ textAlign: 'center', borderTop: 'solid 1px white', margin: '0', paddingTop: '1rem' }}>37 Crescent Ave</p>
+          <p style={{ textAlign: 'center', borderBottom: 'solid 1px white', paddingBottom: '1rem' }}>Plymouth, MA 02360</p>
           {close}
         </article>
 
