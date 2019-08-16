b58f3df5c15baf9b49b1b721d5bf3286e106c311
Spencer Marx <spencer.s.marx@gmail.com>
2019-08-16 09:34:53 -0400

    Modify CompletedForm message

--- src/components/CompletedForm.js
+++ src/components/CompletedForm.js
@@ -6,8 +6,8 @@ class CompletedForm extends Component {
         return (
             <React.Fragment>
                 <div className="CompletedForm">
-                    <h5 className="CompletedForm-Message">Message Sent</h5>
-                    <p className="CompletedForm-SubMessage">We'll be in touch with you soon!</p>
+                    <h5 className="CompletedForm-Message">RSVP Sent</h5>
+                    <p className="CompletedForm-SubMessage">Have a great day and we hope to see you there!</p>
                     <svg className="CompletedForm-Checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle className="CompletedForm-Checkmark-Circle" cx="26" cy="26" r="25" fill="none"/><path className="CompletedForm-Checkmark-Check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg>
                 </div>
             </React.Fragment>
